e9a8be471ce69e40ccf3dc3317c3ff12ff3aedd0
Toni Ramon <toniramon@MacBook-Pro-de-Toni.local>
2019-01-08 18:12:18 +0100

    added repr

--- item.py
+++ item.py
@@ -17,6 +17,9 @@ class Item(Updatable):
     def getQuality(self):
         return self.quality
 
+    def __repr__(self):
+        return '%s, %s, %s' % (self.name, self.sellIn, self.quality)
+
 if __name__ == "__main__":
 
 
