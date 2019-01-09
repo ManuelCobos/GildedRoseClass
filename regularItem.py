71c4c66b566062b8b531352fcfc4f0fefec06414
Toni Ramon <toniramon@MacBook-Pro-de-Toni.local>
2019-01-09 10:30:04 +0100

    regular item refactored

--- regularItem.py
+++ regularItem.py
@@ -4,21 +4,27 @@ from updatable import Updatable
 
 class RegularItem(Item, Updatable):
 
+    def setQuality(self, value):
+        if self.quality + value > 50:
+            self.quality = 50
+        elif self.quality + value >= 0:
+            self.quality += value
+        else:
+            self.quality = 0
+        assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
+        
+
     def update_quality(self):
         if self.sellIn > 0:
-            self.quality -= 1 
+            self.setQuality(-1)
         else: 
-            self.quality -= 2
-        
-        if self.quality < 0:
-            self.quality = 0
-        elif self.quality > 50:
-            self.quality = 50
+            self.setQuality(-2)
         
 
 # Casos test para las diferentes actualizaciones.
 if __name__ == "__main__":
     
+    # Pato = Name, SellIn, Quality
     pato = RegularItem("Pato", 100, 10)
     pato.update_quality()
     assert pato.getQuality() == 9
